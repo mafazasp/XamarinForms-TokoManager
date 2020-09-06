d8e76f428c30e997e647ba8bfc86400a3dfccd63
mafazasp <mafazasp@RETROBEAST>
2020-09-06 15:28:34 +0700

    Sucessfully retrieve User  Properties

--- TokoManager/TokoManager/Views/SignUpPage.xaml.cs
+++ TokoManager/TokoManager/Views/SignUpPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TokoManager.ViewModels;
+using TokoManager.Models;
+using System.Reflection;
 
 namespace TokoManager.Views
 {
@@ -16,9 +18,19 @@ namespace TokoManager.Views
         SignUpVM signUpVM;
         public SignUpPage()
         {
+            
             InitializeComponent();
             signUpVM = new SignUpVM();
             BindingContext = signUpVM;
+
+            var userProperties = new List<String>();
+            Type type = typeof(User);
+            PropertyInfo[] propertyinfo = type.GetProperties();
+            foreach(PropertyInfo info in propertyinfo)
+            {
+                userProperties.Add(info.Name);
+            }
+            signUpForm.ItemsSource= userProperties;
         }
     }
 }
